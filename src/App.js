f085bc797a890304c9d1378af0300ee53c669eea
Karthik Reddy <109334997+KarthikReddy8809@users.noreply.github.com>
2024-05-26 12:17:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Menu from './components/Menu';
+function App() {
+    return ( 
+        <div>
+            < Navbar />
+            <Menu />
+        </div>
+     );
+}
+
+export default App;
