76d240b9235a5eda9dee71b3f5d17da8f6056653
Karthik Reddy <109334997+KarthikReddy8809@users.noreply.github.com>
2024-05-26 12:11:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import Menu from './components/Menu';
-import Cart from './components/Cart';
-import {provider} from "react-redux";
-import store from './Store';
+import Order from "./Order";
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <provider store={store}>
-    <Menu />
-  </provider>
-  
+ <BrowserRouter>
+ <Routes>
+  <Route path ="/" element = {<App />}>
+
+  </Route>
+  <Route path="/order" element ={<Order />}></Route>
+ </Routes>
+ </BrowserRouter>
+ 
 );
 
